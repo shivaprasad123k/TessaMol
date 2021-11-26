b8c7d1ca53f70bda613c5d6ab5451fe1bea3511c
naveennoname07 <85923611+naveennoname07@users.noreply.github.com>
2021-11-26 10:45:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -709,7 +709,7 @@ async def advantage_spell_chok(msg):
             ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spelling#{user}#close_spellcheck')])
     aAdd f" then {msg.text}
-    await msg.reply(f"I couldn't find anything related to that\nDid you mean any one of these?{msg.text}", reply_markup=InlineKeyboardMarkup(btn))
+    await msg.reply(f"I couldn't find anything related to that Movie , Did you mean any one of these below 👇 ?{msg.text}", reply_markup=InlineKeyboardMarkup(btn))
     
 
 
